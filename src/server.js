3fa849e303d583c4aaebdb57c75abcdc2b9210aa
AbhinavDegala <abhinav.degala@gmail.com>
2021-10-09 09:43:05 +0530

    feat: added prometheus client

--- src/server.js
+++ src/server.js
@@ -2,11 +2,17 @@ const http = require('http');
 const moment = require('moment-timezone');
 const app = require('./app');
 const config = require('./config');
+const client = require('prom-client');
+const register = new client.Registry();
 
 const server = http.createServer(app);
 
 server.listen(config.port, () => {
   moment.tz.setDefault('UTC');
   console.log(`Server running on port ${config.port}`);
-
 });
+
+register.setDefaultLabels({
+  app: 'dreamstock-app'
+})
+
