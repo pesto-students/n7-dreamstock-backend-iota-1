d47567c6888f12bd9c9469fad7a1e1b2e31f167f
AbhinavDegala <abhinav.degala@gmail.com>
2021-10-09 08:46:04 +0530

    fix: setup env variables

--- src/config/index.js
+++ src/config/index.js
@@ -3,10 +3,10 @@ dotenv.config();
 // module.exports = {
 //   stockApieEndPoint: process.env.API_URL,
 //   stockApiKey: process.env.STOCK_API_KEY,
-//   port: process.env.PORT,
+//   port: process.env.SERVER_PORT,
 //   mongoUrl: process.env.MONGO_URL,
-//   secredtJWT :process.env.secredtJWT,
-//   demoENV:true
+//   secredtJWT :process.env.SECRET_JWT,
+//   isDemoENV:process.env.IS_DEMO_ENV
 // };
 
 module.exports = {
@@ -15,5 +15,5 @@ module.exports = {
   port: 5000,
   mongoURI: 'mongodb+srv://new-user1:KsXJ2rZyWjrB9Wr9@cluster0.bdyyh.mongodb.net/DreamStock?retryWrites=true&w=majority',
   secredtJWT: 'secret',
-  demoENV: true,
+  isDemoENV: true,
 };
